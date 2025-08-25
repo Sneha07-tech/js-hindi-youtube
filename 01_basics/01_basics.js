8b3c9cbf9cf2f17df1150e84dd60c905b59a9275
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-08-25 09:47:16 +0000

    datatypes

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,20 @@
+const accountId=12234
+let accountEmail="hitesh1123@gmail.com"
+var accountPassword="12455"
+accountCity="jaipur"
+let accountState;   
+// undefined
+
+// accountId=2 not allowed
+console.log(accountId);
+accountEmail="hsds@njde.com"
+accountPassword="42435"
+accountCity="bengaluru"
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var
+because of issue i block scope and functional scope
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newwr version of javascript
+
+// alert(3+3)//we are using nodejs,not browser
+console.log(3+3)//code readibility should be high
+console.log("hitesh")
+let name="hitesh" 
+let age=18
+let isLoggedIn=false
+
+
+// number=>2 to power 53
+// bigint
+// srting=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique
+// boject
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
