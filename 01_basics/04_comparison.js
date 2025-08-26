98db2c9b8fa96fba2ad9f0492aa47fad43d2ebf2
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-08-26 06:03:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+console.log(null>0);
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined==0)
+console.log(undefined>0);
+console.log(undefined<0)
+
+// this type of conversion are quite inconsistent
+
+
+// === this is a strict check means it check values and datatype
+// it i more reliable
+console.log("2"===2)
