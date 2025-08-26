e7351be83e8b2a531fd7386afad0924a41847f41
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-08-26 05:40:29 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,3 +18,29 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof(stringNumber))
+
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+console.log(2+2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%3)
+ 
+let str1="hello"
+let str2=" hitesh"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(2+2+"1")
+console.log((3+4)*5%3);
+console.log(+true)
+console.log(+"")
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+
+
