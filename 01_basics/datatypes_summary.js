1164ec5d601dea5ff2581b39095c670e9878de04
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-08-27 05:41:21 +0000

    datatypes-summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+// primitive
+// 7types:String,Number,Boolean,null,undefined,Symbol,BigInt
+const score=100
+const scorevalue=100.3
+const isLoggedIn=false
+const outsideTemp=null 
+let useremail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId)
+
+const bigNumber=34445884832454n
+
+
+ 
+// Reference(non primitive)
+// Arrays,objects,functions
+
+const heroes=["shaktiman","nnagraj","doga"]
+let myObject={
+    name:"hitesh",
+    age:22,
+
+}
+const myFunction=function(){
+    console.log("hello World");
+}
+console.log(typeof myFunction)
+console.log(id)
