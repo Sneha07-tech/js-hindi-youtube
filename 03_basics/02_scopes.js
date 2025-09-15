afd8ed7ee1d4a7e824d271164cedc75eaaadce22
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-09-15 13:41:59 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,50 @@
+var c=300
+if(true){
+    let a=10
+    const b=20
+    var c=30
+}
+// console.log(a);
+// console.log(b)
+// console.log(c)
+// for(let i=0;i<array.length;i++){
+//     const element=array[i];
+// }
+function one(){
+    const username="hitesh"
+    function two(){
+        const website="youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website=" youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username)
+
+
+
+
+
+
+
+
+// +++++++++++++++++++Interesting+++++++++++
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+const addTwo=function(num){
+    return num+2
+}
+console.log(addTwo(5))
