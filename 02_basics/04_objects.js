4825f7082c8a53c6749a75d6a2ea0015b8cc5597
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-09-15 10:21:05 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -14,13 +14,13 @@ const regularuser={
         }
     }
 }
-console.log(regularuser.fullname.userfullname.firstname)
+// console.log(regularuser.fullname.userfullname.firstname)
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 // const obj3={obj1,obj2}
 // const obj3=Object.assign({},obj1,obj2)
 const obj3={...obj1,...obj2}
-console.log(obj3);
+// console.log(obj3);
 const users=[
     {
         id:1,
@@ -39,10 +39,29 @@ const users=[
 
 ]
 users[1].email
-console.log(tinderuser);
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));
-console.log(Object.entries(tinderuser));
-console.log(tinderuser.hasOwnProperty('isLogged'))
+// console.log(tinderuser);
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+// console.log(tinderuser.hasOwnProperty('isLogged'))
 
-
+const course={
+ coursename:"js in hindi",
+ price:"999",
+ courseInstructor:"hitesh"   
+}
+// course.courseInstructor
+const {courseInstructor}=course
+console.log(courseInstructor)
+const {courseInstructor:Instructor}=course
+console.log(Instructor)
+//  {
+//     "name":"hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+// // [
+// {},
+// {},
+// {},
+// ]
