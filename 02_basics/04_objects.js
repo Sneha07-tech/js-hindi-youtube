5fbf62739e3492fb3a24bf8019348237a6d2f244
Sneha07-tech <snehajaiswal231223@gmail.com>
2025-09-15 07:21:22 +0000

    object2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,48 @@
+// const tinderuser=new Object()
+const tinderuser={}
+tinderuser.id="123abc"
+tinderuser.name="sammy"
+tinderuser.isLoggedIn=false
+
+// console.log(tinderuser);
+const regularuser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"hitesh",
+            lastname:"choudhary"
+        }
+    }
+}
+console.log(regularuser.fullname.userfullname.firstname)
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+// const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2)
+const obj3={...obj1,...obj2}
+console.log(obj3);
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+
+   {
+        id:1,
+        email:"h@gmail.com"
+    },
+        {
+        id:1,
+        email:"h@gmail.com"
+    },
+
+
+]
+users[1].email
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+console.log(tinderuser.hasOwnProperty('isLogged'))
+
+
